1ca79aa03d037196cdd4560b080055301d8f2004
rakshith291 <rakshith291@gmail.com>
2020-11-29 14:13:42 -0500

    Update data.py

--- data.py
+++ data.py
@@ -9,12 +9,12 @@ class Data:
             [transforms.ToTensor(),
              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-        trainset = torchvision.datasets.CIFAR10(root='/home/aer4kor/PycharmProjects/PP/cifar10_classification_pytorch/train', train=True,
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                                 download=True, transform=transform)
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                                   shuffle=True, num_workers=2)
 
-        testset = torchvision.datasets.CIFAR10(root='/home/aer4kor/PycharmProjects/PP/cifar10_classification_pytorch/test', train=False,
+        testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                                download=True, transform=transform)
         testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                                  shuffle=False, num_workers=2)
